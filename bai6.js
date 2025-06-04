8b9b9aabc644879470922960873e54c4ca2927cb
thang hoang <thanghq12@fpt.edu.vn>
2025-06-04 08:24:26 +0700

    update

--- bai6.js
+++ bai6.js
@@ -6,3 +6,10 @@ function tinhToan() {
    let tong = so1 + so2;
     alert(tong);
 }
+// tạo 2 ô input chieudai,chieurong 
+// tạo 2 nút tính chu vi,tính diện tích 
+// tạo 3 ô input cho nhập vào a,b,c
+// giải phương trình ax^2+bx+c và hiển thị ra nghiệm 
+// tạo 3 ô input đáy lớn, đáy bé,chiều cao
+// tính diện tích hình thang 
+//ancncnc
