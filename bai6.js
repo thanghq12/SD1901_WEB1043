3f4b7141dd28e1a180ea3174ca4d8423b63635ba
thang hoang <thanghq12@fpt.edu.vn>
2025-06-04 08:21:51 +0700

    update

---
+++ bai6.js
@@ -0,0 +1,8 @@
+function tinhToan() {
+    // alert("Test nut");
+    // lấy giá trị của nut 1
+   let so1 = parseInt(document.getElementById('so1').value);
+   let so2 = parseInt(document.getElementById('so2').value);
+   let tong = so1 + so2;
+    alert(tong);
+}
